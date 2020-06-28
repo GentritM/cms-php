2638eda68f12e22c8a984e15826a15d6918c66a6
gentim <genti_mehmeti@outlook.com>
2020-06-28 16:11:26 +0100

    added files

---
+++ cms/includes/db.php
@@ -0,0 +1,24 @@
+<?php 
+
+
+$db['db_host'] = "localhost";
+$db['db_user'] = "master";
+$db['db_password'] = "America7778.";
+$db['db_name'] = "cms";
+
+foreach($db as $key => $value){
+
+	define(strtoupper($key), $value);
+
+}
+
+$connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+
+if($connection){
+
+	
+
+} 
+
+
+?>
